b2612c7251a8dfb2d5229e57c9e3b8be8d815936
Steven <github@steven-h.net>
2019-06-28 15:04:39 -0400

    Continued

--- src/main/java/net/nixill/dice/objects/DCDie.java
+++ src/main/java/net/nixill/dice/objects/DCDie.java
@@ -2,12 +2,21 @@ package net.nixill.dice.objects;
 
 public class DCDie extends DCSingle {
   public DCDie(double sides, double value) {
-    super(value, sides);
+    sides = Math.floor(sides);
+    potential = sides;
+    
+    this.value = value;
   }
 
   public DCDie(double sides) {
+    sides = Math.floor(sides);
+    potential = sides;
     if (sides >= 2) {
-      
+      value = (double) (Randomizer.nextInt((int) sides + 1));
+    } else if (sides == 1) {
+      value = Randomizer.nextDouble();
+    } else {
+      throw new IllegalArgumentException("Dice must have at least one side.");
     }
   }
 }
