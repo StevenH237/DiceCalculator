af90595660e564645dbf3b0ab5cfd8c4a3e1b21c
ShadowFoxNixill <github@nix.nixill.net>
2019-07-13 19:15:47 -0400

    wwork

--- src/main/java/net/nixill/dice/operations/SavedFunctions.java
+++ src/main/java/net/nixill/dice/operations/SavedFunctions.java
@@ -9,11 +9,25 @@ import net.nixill.dice.objects.DCEntity;
  * A class that provides access to save and load {@link Function}s.
  */
 public class SavedFunctions {
-  private static FunctionLoader                              loader;
+  private static FunctionLoader loader;
   private static ThreadLocal<ArrayList<ArrayList<DCEntity>>> params;
-  
+
   public static void setLoader(FunctionLoader loader) {
     SavedFunctions.loader = loader;
   }
-  
+
+  private static ArrayList<ArrayList<DCEntity>> getStack() {
+    return params.get();
+  }
+
+  public static void stackParams(ArrayList<DCEntity> pars) {
+    getStack().add(pars);
+  }
+
+  public static void unstackParams() {
+    ArrayList<ArrayList<DCEntity>> stack = getStack();
+    if (stack.size() > 0) {
+      stack.remove(stack.size() - 1);
+    }
+  }
 }
