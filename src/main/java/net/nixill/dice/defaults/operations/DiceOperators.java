dfb6110750d31fcfeef99d4961b8b83d7036affa
ShadowFoxNixill <github@nix.nixill.net>
2019-07-17 10:58:39 -0400

    more

--- src/main/java/net/nixill/dice/defaults/operations/DiceOperators.java
+++ src/main/java/net/nixill/dice/defaults/operations/DiceOperators.java
@@ -11,35 +11,43 @@ import net.nixill.dice.operations.ComparisonOperators;
 import net.nixill.dice.operations.PrefixOperator;
 
 public class DiceOperators {
-  public static final BinaryOperator<DCList> DICE = new BinaryOperator<>("d", Priorities.DICE, (left, right) -> {
-    double count = Math.floor(left.getValue().getSingle().getAmount());
-    double sides = Math.floor(right.getValue().getSingle().getAmount());
-
-    if (count < 1) {
-      throw new DiceCalcException(new IllegalArgumentException("You must roll at least one die."));
-    }
-
-    if (sides < 1) {
-      throw new DiceCalcException(new IllegalArgumentException("Dice must have at least one side."));
-    }
-
-    ArrayList<DCValue> out = new ArrayList<>();
-    for (int i = 0; i < count; i++) {
-      out.add(new DCDie(sides));
-    }
-
-    return new DCList(out);
+  public static final BinaryOperator<DCList> DICE = new BinaryOperator<>(
+      "d", Priorities.DICE, (left, right) -> {
+        double count = Math.floor(left.getValue().getSingle().getAmount());
+        double sides = Math
+            .floor(right.getValue().getSingle().getAmount());
+        
+        if (count < 1) {
+          throw new DiceCalcException(new IllegalArgumentException(
+              "You must roll at least one die."));
+        }
+        
+        if (sides < 1) {
+          throw new DiceCalcException(new IllegalArgumentException(
+              "Dice must have at least one side."));
+        }
+        
+        ArrayList<DCValue> out = new ArrayList<>();
+        for (int i = 0; i < count; i++) {
+          out.add(new DCDie(sides));
+        }
+        
+        return new DCList(out);
+      });
+  
+  public static final PrefixOperator<DCDie> ONE_DIE = new PrefixOperator<>(
+      "d", Priorities.DICE, (ent) -> {
+        double sides = Math.floor(ent.getValue().getSingle().getAmount());
+        
+        if (sides < 1) {
+          throw new DiceCalcException(new IllegalArgumentException(
+              "Dice must have at least one side."));
+        }
+        
+        return new DCDie(sides);
+      });
+  
+  public static final ComparisonOperators<DCList> ROLL_UNTIL = new ComparisonOperators<>("u", Priorities.DICE, (left, comp, right) -> {
+    double sides = 
   });
-
-  public static final PrefixOperator<DCDie> ONE_DIE = new PrefixOperator<>("d", Priorities.DICE, (ent) -> {
-    double sides = Math.floor(ent.getValue().getSingle().getAmount());
-
-    if (sides < 1) {
-      throw new DiceCalcException(new IllegalArgumentException("Dice must have at least one side."));
-    }
-
-    return new DCDie(sides);
-  });
-
-  public static final ComparisonOperators<DCList> ROLL_UNTIL = new ComparisonOperators<>("u", Priorities.DICE, func)
 }
